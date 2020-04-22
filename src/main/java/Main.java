8bb74389779ae226cb4be5ad43289357a771f600
Kenta Kawasaki <kenta.kawasaki@gmo.media>
2020-04-22 18:39:12 +0900

    解答例

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,51 @@
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.stream.Stream;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
-        System.out.println(Paths.get("./src/main/resources/score.csv").toFile().getAbsolutePath());
-        System.out.println(Paths.get(".").toFile().getAbsolutePath());
+        Main main = new Main("score.csv");
+        main.execute();
+    }
 
-        Stream<String> stream = Files.lines(Paths.get("score.csv"));
-        System.out.println(stream);
+    private List<Student> students;
 
+    public Main(String filename) {
+        try {
+            students = Files.lines(Paths.get(filename))
+                    .map(s -> s.split(","))
+                    .map(Student::new)
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void execute() {
+        viewTop();
+        viewAvg();
+    }
+
+    public void viewTop() {
+        Optional<String> first = students
+                .stream()
+                .sorted(Comparator.comparing(Student::getEnglishScore, Comparator.reverseOrder()))
+                .map(Student::getName)
+                .findFirst();
+        System.out.println("英語: " + first.orElse("該当なし"));
+    }
+
+    public void viewAvg() {
+        OptionalDouble average = students
+                .stream()
+                .mapToDouble(Student::getEnglishScore)
+                .average();
+        System.out.println(average.orElse(0));
     }
 }
